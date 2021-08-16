25eb6e9f99c5ab079f82a4a346df4526650668a0
Michael Douglas Soares <michaeldouglas.10.94@gmail.com>
2021-08-16 14:30:36 -0300

    list left right

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -6,9 +6,9 @@ function Footer()
 {
     return (
         <footer>
-            <p>Desenvolvido clone <span>NETFLIX</span> afim de estudos frontend.</p>
+            <p>Desenvolvido clone <a id="first" href="https://netflix.com.br">NETFLIX</a> afim de estudos frontend.</p>
             <p>Michael Douglas Soares</p>
-            <p>ireitos imagem Netlix</p>
+            <p>Direitos imagem Netlix</p>
             <p>API utilizada: <a href="https://www.themoviedb.org/">TMBD.</a></p>
         </footer>
     )
--- src/pages/ListMovies/index.js
+++ src/pages/ListMovies/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import './style.css';
 
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
 function ListMovies({ title, movie })
 {
     return (
         <div className="movie-row">
             <h2>{title}</h2>
+            <div className="movie-row-left">
+                <NavigateBeforeIcon style={{ fontSize: 50 }} />
+            </div>
+            <div className="movie-row-right">
+                <NavigateNextIcon style={{ fontSize: 50 }} />
+            </div>
             <div className="movie-row-list-area">
                 <div className="movie-row-list">
                     {movie.results.length > 0 && movie.results.map((item, key) => (
